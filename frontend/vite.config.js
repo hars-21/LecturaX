cef083a0e9a1adc5feb2ab7e687e065076597b2a
Harshil <harshilgupta.2005@gmail.com>
2025-08-13 23:48:58 +0530

    added deployment url

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
     open: true,
     proxy: {
       "/api": {
-        target: "http://localhost:5000",
+        target: "https://thinkdock-api.onrender.com",
         changeOrigin: true,
         secure: false,
       },
